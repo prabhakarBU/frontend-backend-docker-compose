691d9c649479bad8b1d7e6a8e330707a73b562d8
prabhakarBU <psharma3@binghamton.edu>
2023-04-25 19:33:54 -0600

    adding frontend and backend

---
+++ node/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      "id":"1",
+      "comment":"where to go from here?"
+    },
+    {
+      "id":"2",
+      "comment":"Couldnt' be any better!"
+    },
+    {
+      "id":"3",
+      "comment":"should've been watching Netflix more"
+    }
+  ])
+})
+
+app.listen(4000, () => {
+  console.log('connected on port 4000')
+})
